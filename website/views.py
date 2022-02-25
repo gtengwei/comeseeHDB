c1fe619f57d1c4de7913c36dbb5f3e3419f47022
NanShiYuan <58169338+ShiYuanNan@users.noreply.github.com>
2022-02-26 00:26:28 +0800

    1

--- website/views.py
+++ website/views.py
@@ -65,7 +65,7 @@ def flat_details(flatId):
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    os.chdir("C:/Users/tengwei/Desktop/github/comeseeHDB/website")
+    os.chdir("/Users/nanshiyuan/Documents/GitHub/website")
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     myquery = ("SELECT id, street_name, resale_price,flat_type, storey_range FROM Flat;")
