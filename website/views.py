ccde6cddbd042e5f18006af7e76d7e9c8a57daab
NanShiYuan <58169338+ShiYuanNan@users.noreply.github.com>
2022-02-25 22:57:55 +0800

    fetched from tw

--- website/views.py
+++ website/views.py
@@ -1,17 +1,18 @@
 ## For creation of stuff that can be viewed on homepage
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for, redirect
 from flask_login import login_required, current_user
 from .models import *
 from . import db
 import json
+import sqlite3
 
 views = Blueprint('views', __name__)
 
 ## To be changed to placeholders/HDB flats
 ## To be completed
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/reviews', methods=['GET', 'POST'])
 @login_required
-def home():
+def review():
     if request.method == 'POST':
         review = request.form.get('review')
 
@@ -25,10 +26,10 @@ def home():
             db.session.commit()
             flash('Review added!', category='success')
 
-    return render_template("home.html", user=current_user)
+    return render_template("review.html", user=current_user)
 
 ## 
-@views.route('/delete-review', methods=['POST'])
+@views.route('/delete-review', methods=['GET','POST'])
 def delete_review():
     review = json.loads(request.data)
     reviewId = review['reviewId']
@@ -36,6 +37,56 @@ def delete_review():
     if review:
         if review.user_id == current_user.id:
             db.session.delete(review)
+            flash('Review deleted!', category='success')
             db.session.commit()
-
     return jsonify({})
+
+
+@views.route('/flat-details/<flatId>', methods=['GET', 'POST'])
+@login_required
+def flat_details(flatId):
+    flat = Flat.query.filter_by(id=flatId).first_or_404()
+    if request.method == 'POST':
+        review = request.form.get('review')
+
+        if len(review) < 1:
+            flash('Review is too short!', category='error')
+        elif len(review) > 500:
+            flash('Review is too long! Maximum length for a review is 500 characters', category='error')
+        else:
+            new_review = Review(data=review, user_id=current_user.id, flat_id = flatId)
+            db.session.add(new_review)
+            db.session.commit()
+            flash('Review added!', category='success')
+    return render_template("flat_details.html", user=current_user, flat = flat)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    os.chdir("/Users/nanshiyuan/Documents/Github/website")
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    myquery = ("SELECT id, street_name, resale_price,flat_type, storey_range FROM Flat;")
+    c.execute(myquery)
+    data=list(c.fetchall())
+
+    return render_template('home.html', user=current_user,data=data[:15])
+
+@views.route('/api', methods=['GET', 'POST'])
+def api():
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    myquery = ("SELECT id, street_name, resale_price,flat_type, storey_range FROM Flat;")
+    c.execute(myquery)
+    data=list(c.fetchall())
+
+    if request.args:
+        index = int(request.args.get('index'))
+        limit = int(request.args.get('limit'))
+    
+        return jsonify({'data': data[index:limit + index]})
+    else:
+        return jsonify({'data': data})
+
+
