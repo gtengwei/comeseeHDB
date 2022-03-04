846c0bc733dd62addc84813ff1da01fa9c1c885c
iamhannah <hereismytrashbin@gmail.com>
2022-03-04 21:46:33 +0800

    added more files required to deploy app

--- website/views.py
+++ website/views.py
@@ -63,7 +63,7 @@ def flat_details(flatId):
 ## Route for Home Page
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    os.chdir("C:/Users/tengwei/Desktop/github/comeseeHDB/website")   
+    os.chdir("C:/Users/Hannah V/Documents/GitHub/comeseeHDB/website")   
     #os.chdir("website") 
     #print(os.getcwd())
     conn = sqlite3.connect("database.db")
@@ -132,7 +132,7 @@ def home():
 ## Infinte Scrolling for Home Page
 @views.route('/load_home', methods=['GET', 'POST'])
 def load_home():
-    os.chdir("C:/Users/tengwei/Desktop/github/comeseeHDB/website")
+    os.chdir("C:/Users/Hannah V/Documents/GitHub/comeseeHDB/website")
     print(os.getcwd())
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
