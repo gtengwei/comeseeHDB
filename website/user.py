b3ded8a710b2e6c67649ef4066fceb1497dd61c7
gtengwei <67195159+gtengwei@users.noreply.github.com>
2022-04-17 18:58:12 +0800

    final commit

--- website/user.py
+++ website/user.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from .models import *
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from flask_login import login_user, login_required, logout_user, current_user
-from datetime import datetime, timedelta
+from flask_login import login_required, current_user
+from datetime import datetime
 from .misc import *
 
 user = Blueprint('user', __name__)
