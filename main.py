8e9d14ea613ee112ada88455cfa1b95bc06e456f
cheryltyx <cheryltyx17@gmail.com>
2022-08-30 02:59:25 +0800

    .

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-## To start the app
+# To start the app
 #from website import create_app
 from website import *
 from website.models import *
-from website.__init__ import app 
-app = app
+from website.test import *
+
 #
 if __name__ == '__main__':
     #app = create_app()
+    # create_HDB_Flats_table()
+    # create_Flat_table()
+    # db.create_all(app=app)
     app.run(debug=True)
-
-
