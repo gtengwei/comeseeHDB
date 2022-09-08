709e95eacdabf6f31006315b29aa077f2d433262
gtengwei <67195159+gtengwei@users.noreply.github.com>
2022-09-09 00:27:32 +0800

    fixed main branch and deleted mysql and not used code

--- website/__init__.py
+++ website/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from .models import *
+from werkzeug.security import generate_password_hash
 
 DB_NAME = "database.db"
 login_manager = LoginManager()
@@ -27,11 +28,11 @@ def create_database(app):
 # def create_app():
 app = Flask(__name__)
 
-
+create_database(app)
 app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Clutch123!@localhost/mysql_database?charset=utf8'
-#create_database(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # To send reset password email to user
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
@@ -53,7 +54,6 @@ app.register_blueprint(auth, url_prefix='/')
 app.register_blueprint(user, url_prefix='/')
 
 # return app
-
 
 @login_manager.user_loader
 def load_user(id):
